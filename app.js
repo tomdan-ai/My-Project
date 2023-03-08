bab1ca1e4b605762b8823ecb911008a3b82d8939
Tom Daniel <tomudoh258@gmail.com>
2023-03-08 17:06:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-function myElement(selector) {
-  return document.querySelector(selector);
-}
-
-const menu = myElement("#hamma");
-const navbar = myElement(".mobile-nav");
-const closeNav = myElement(".close-btn");
-const choose = myElement(".mobile-nav");
-
-function addMenu(selected) {
-  return selected.addEventListener("click", () =>
-    navbar.classList.add("nav-toggle")
-  );
-}
-
-function removeMenu(selected) {
-  return selected.addEventListener("click", () =>
-    navbar.classList.remove("nav-toggle")
-  );
-}
-
-addMenu(menu);
-removeMenu(closeNav);
-removeMenu(choose);
-
 // WORKS SECTION
 const workSection = document.querySelector("#work-section");
 
