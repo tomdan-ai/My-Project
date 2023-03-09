7ec625e1d8ccad032a8bd61aa812d1608bc30695
Tom Daniel <tomudoh258@gmail.com>
2023-03-09 12:08:59 +0100

    Add menu

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+// HAMBUGGER MENU
+
+function myElement(selector) {
+  return document.querySelector(selector);
+}
+
+const menu = myElement('#hamma');
+const navbar = myElement('.mobile-nav');
+const closeNav = myElement('.close-btn');
+const choose = myElement('.mobile-nav');
+
+function addMenu(selected) {
+  return selected.addEventListener('click', () => navbar.classList.add('nav-toggle'));
+}
+
+function removeMenu(selected) {
+  return selected.addEventListener('click', () => navbar.classList.remove('nav-toggle'));
+}
+
+addMenu(menu);
+removeMenu(closeNav);
+removeMenu(choose);
+
 // FORM VALIDATION
 const contactForm = document.getElementById('contact-form');
 contactForm.addEventListener('submit', (e) => {
